2b1bcdbd6aaab219cc47175a99fd789ec27fc07e
Laura Luchiari <95256189+lauraluch@users.noreply.github.com>
2023-10-17 20:24:46 -0300

    feat: enable and create ActivityMainBinding

--- app/src/main/java/com/laura/splitthebill/view/MainActivity.kt
+++ app/src/main/java/com/laura/splitthebill/view/MainActivity.kt
@@ -3,8 +3,12 @@ package com.laura.splitthebill.view
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.laura.splitthebill.R
+import com.laura.splitthebill.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private val amb: ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
