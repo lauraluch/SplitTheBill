c1161c4edaa5bcc0543f19d2a6fa6b47cd6f6fb1
Laura Luchiari <95256189+lauraluch@users.noreply.github.com>
2023-10-24 19:55:47 -0300

    feat: create class PersonDaoImpl to implement Dao methods

---
+++ app/src/main/java/com/laura/splitthebill/model/PersonDaoImpl.kt
@@ -0,0 +1,25 @@
+package com.laura.splitthebill.model
+
+import android.content.Context
+
+class PersonDaoImpl(context: Context) : PersonDao {
+    override fun createPerson(person: Person): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun readPerson(id: Int): Person? {
+        TODO("Not yet implemented")
+    }
+
+    override fun readPeople(): MutableList<Person> {
+        TODO("Not yet implemented")
+    }
+
+    override fun updatePerson(person: Person): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun deletePerson(id: Int): Int {
+        TODO("Not yet implemented")
+    }
+}
