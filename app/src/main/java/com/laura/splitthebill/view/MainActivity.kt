c085dfabbbb0f73e8d9f994c9b43125f50810029
lauraluch <lauraluchiari@hotmail.com>
2023-10-30 18:30:54 -0300

    refactor: instantiate a people list and a personController in mainActivity

--- app/src/main/java/com/laura/splitthebill/view/MainActivity.kt
+++ app/src/main/java/com/laura/splitthebill/view/MainActivity.kt
@@ -3,12 +3,21 @@ package com.laura.splitthebill.view
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.laura.splitthebill.R
+import com.laura.splitthebill.controller.PersonController
 import com.laura.splitthebill.databinding.ActivityMainBinding
+import com.laura.splitthebill.model.Person
 
 class MainActivity : AppCompatActivity() {
     private val amb: ActivityMainBinding by lazy {
         ActivityMainBinding.inflate(layoutInflater)
     }
+
+    private val peopleList: MutableList<Person> = mutableListOf()
+
+    private val personController: PersonController by lazy {
+        PersonController(this)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
