1b03495e984f8b77b7e28fc9780b33a10c10620f
lauraluch <lauraluchiari@hotmail.com>
2023-10-30 17:33:38 -0300

    feat: create onCreate function on PersonDetailsActivity

--- app/src/main/java/com/laura/splitthebill/view/PersonDetailsActivity.kt
+++ app/src/main/java/com/laura/splitthebill/view/PersonDetailsActivity.kt
@@ -2,6 +2,7 @@ package com.laura.splitthebill.view
 
 import android.os.Bundle
 import android.os.PersistableBundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.laura.splitthebill.databinding.PersonDescBinding
 import com.laura.splitthebill.model.Constant.EXTRA_PERSON
@@ -13,4 +14,22 @@ class PersonDetailsActivity: AppCompatActivity() {
         PersonDescBinding.inflate(layoutInflater)
     }
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(apdb.root)
+
+        val receivedPerson = intent.getParcelableExtra<Person>(EXTRA_PERSON)
+        receivedPerson?.let { _receivedPerson ->
+            val viewPerson: Boolean = intent.getBooleanExtra(VIEW_PERSON, false)
+            with(apdb) {
+                if(viewPerson) {
+                    nameEt.isEnabled = false
+                    totalPriceEt.isEnabled = false
+                    saveBt.visibility = View.GONE
+                }
+                nameEt.setText(_receivedPerson.name)
+                totalPriceEt.setText(_receivedPerson.totalPricePaid.toString())
+            }
+        }
+    }
 }
