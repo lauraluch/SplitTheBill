a2ab0ef74ca713b740c1f906f33fad13f4ce2752
lauraluch <lauraluchiari@hotmail.com>
2023-10-30 18:45:39 -0300

    refactor: implement activity result in function onCreate

--- app/src/main/java/com/laura/splitthebill/view/MainActivity.kt
+++ app/src/main/java/com/laura/splitthebill/view/MainActivity.kt
@@ -1,11 +1,16 @@
 package com.laura.splitthebill.view
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import com.laura.splitthebill.R
 import com.laura.splitthebill.adapter.PersonAdapter
 import com.laura.splitthebill.controller.PersonController
 import com.laura.splitthebill.databinding.ActivityMainBinding
+import com.laura.splitthebill.model.Constant.EXTRA_PERSON
+import com.laura.splitthebill.model.Constant.VIEW_PERSON
 import com.laura.splitthebill.model.Person
 
 class MainActivity : AppCompatActivity() {
@@ -26,8 +31,39 @@ class MainActivity : AppCompatActivity() {
         )
     }
 
+    private lateinit var parl: ActivityResultLauncher<Intent>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(amb.root)
+
+        amb.peopleLv.adapter = personAdapter
+
+        parl = registerForActivityResult(
+            ActivityResultContracts.StartActivityForResult()
+        ){ result ->
+            if (result.resultCode == RESULT_OK){
+                val person = result.data?.getParcelableExtra<Person>(EXTRA_PERSON)
+                person?.let { _person ->
+                    if(peopleList.any { it.id == person.id }){
+                        personController.editPerson(_person)
+                    }else {
+                        personController.insertPerson(_person)
+                    }
+                }
+            }
+        }
+
+        amb.peopleLv.setOnItemClickListener{parent, view, position, id->
+            val person = peopleList[position]
+            val viewPersonIntent = Intent(this, PersonDetailsActivity::class.java)
+                .putExtra(EXTRA_PERSON, person)
+                .putExtra(VIEW_PERSON,true)
+
+            startActivity(viewPersonIntent)
+        }
+
+        registerForContextMenu(amb.peopleLv)
+        personController.getPeople()
     }
 }
