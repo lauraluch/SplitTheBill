48e5489e9e84235bc8359fc06977a71dd2475583
lauraluch <lauraluchiari@hotmail.com>
2023-10-30 17:23:33 -0300

    feat: create constants class

---
+++ app/src/main/java/com/laura/splitthebill/model/Constant.kt
@@ -0,0 +1,7 @@
+package com.laura.splitthebill.model
+
+object Constant {
+    const val EXTRA_PERSON = "EXTRA_PERSON"
+    const val VIEW_PERSON = "VIEW_PERSON"
+    const val INVALID_CONTACT_ID = -1
+}
--- app/src/main/java/com/laura/splitthebill/view/PersonDetailsActivity.kt
+++ app/src/main/java/com/laura/splitthebill/view/PersonDetailsActivity.kt
@@ -1,10 +1,13 @@
 package com.laura.splitthebill.view
 
+import android.os.Bundle
+import android.os.PersistableBundle
 import androidx.appcompat.app.AppCompatActivity
 import com.laura.splitthebill.databinding.PersonDescBinding
+import com.laura.splitthebill.model.Person
 
 class PersonDetailsActivity: AppCompatActivity() {
-    private val acb: PersonDescBinding by lazy {
+    private val apdb: PersonDescBinding by lazy {
         PersonDescBinding.inflate(layoutInflater)
     }
 }
