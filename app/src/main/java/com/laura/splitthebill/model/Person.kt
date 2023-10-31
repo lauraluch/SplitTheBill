200d040abe7f70f084e491a33edd5687eef3107d
Laura Luchiari <95256189+lauraluch@users.noreply.github.com>
2023-10-31 17:28:02 -0300

    refactor: change person class to fit room

--- app/src/main/java/com/laura/splitthebill/model/Person.kt
+++ app/src/main/java/com/laura/splitthebill/model/Person.kt
@@ -1,12 +1,20 @@
 package com.laura.splitthebill.model
 
 import android.os.Parcelable
+import androidx.annotation.NonNull
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 import kotlinx.parcelize.Parcelize
 
 @Parcelize
+@Entity
 data class Person(
+    @PrimaryKey(autoGenerate = true)
     var id: Int?,
+    @NonNull
     var name: String,
+    @NonNull
     var totalPricePaid: Double,
+    @NonNull
     var thingsBought: String
 ): Parcelable
